b295d9f7d4aefb6adc5b4dded0862cf418f4ceda
roy-mamata <155751361+roy-mamata@users.noreply.github.com>
2024-01-14 21:58:19 +0530

    Create cc_device_data_aggregator_handler.hpp
    added the header file that declares 'dataAggregatorHandler'  class

---
+++ code/bl/inc/cc_device_data_aggregator_handler.hpp
@@ -0,0 +1,40 @@
+#ifndef _DATA_AGGREGATOR_HANDLER_H_
+#define _DATA_AGGREGATOR_HANDLER_H_
+
+#include <iostream>
+#include "inc/cc_device_common.hpp"
+#include "inc/cc_device_drawing_box.hpp"
+
+class dataAggregatorHandler
+{
+    private:
+
+    /// @brief Static pointer instance to make this class singleton.
+    static dataAggregatorHandler* m_pInstance;
+
+    public:
+    /// @brief Public static method getInstance(). This function is
+    // responsible for object creation.
+    static dataAggregatorHandler* getInstance();
+
+    /// @brief process the image from folder
+    /// @return returns the detected classes along with coordinates as JSON string
+    bool dataAggregator_output();
+
+    private:
+
+    /// @brief Define constructor in the private section to make this class as singleton.
+    dataAggregatorHandler() = default;
+
+    /// @brief Define destructor in private section, so no one can delete the instance of this class.
+    ~dataAggregatorHandler() = default;
+
+    /// @brief Define copy constructor in the private section, so that no one can voilate the singleton policy of this class
+    dataAggregatorHandler(const dataAggregatorHandler& obj){}
+
+    /// @brief Define assignment operator in the private section, so that no one can voilate the singleton policy of this class
+    void operator=(const dataAggregatorHandler& obj){}
+
+};
+
+#endif // End of _DATA_AGGREGATOR_HANDLER_H_
