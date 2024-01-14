6b1954885f3dd6197449e584fd3e62d35f4e2df9
roy-mamata <155751361+roy-mamata@users.noreply.github.com>
2024-01-14 22:14:29 +0530

    Create cc_device_common.hpp
    Defined the variables for easy maintenance and consistency across the project.

---
+++ code/common/inc/cc_device_common.hpp
@@ -0,0 +1,20 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+const int CONFIDENCE_THRESHOLD =70;
+
+const string file_path1    = "../data/adasv1.json";
+const string file_path2    = "../data/big_model.json";
+
+const string MISSING_DETECTION_PATH = "../data/missing_detection.json";
+const string INPUT_IMAGE = "../data/input.jpeg";
+const string MISSING_DETECTION_IMAGE ="../data/missing_detection.jpg";
+
+#define NULL_PTR 0
+
+#endif // End of _COMMON_H_
